ebe6635b873519d2187abf92e9ec0529429bd089
Gerard Sala <12766134+scripting-drafts@users.noreply.github.com>
2021-10-31 23:01:55 +0100

    Update Google-Places-DNNRegressor.py

--- Google-Places-DNNRegressor.py
+++ Google-Places-DNNRegressor.py
@@ -3,7 +3,6 @@ import branca
 import warnings
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
 from tensorflow.keras.layers import *
